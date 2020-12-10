fe54b4cd9749f83f981a15fd8a017f6e9877caf2
Chi Feng <email.chi.feng@gmail.com>
2020-12-10 13:44:48 +0800

    Clean up day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,22 +1,17 @@
 import numpy as np
-from collections import Counter, defaultdict
+from collections import Counter
+from functools import lru_cache
 
 with open("day10/input.txt") as file:
     a = list(map(int, file.read().split("\n")))
 
 a = sorted([0] + a + [max(a) + 3])
-n = len(a)
-
 c = Counter(np.diff(a))
 print(c[1] * c[3])
 
-# memo[i] is number of ways to reach the end from a[i]
-memo = defaultdict(int)
-memo[n - 1] = 1
-for i in range(n - 2, -1, -1):
-    for j in range(i + 1, n):
-        if a[j] - a[i] <= 3:
-            memo[i] += memo[j]
-        else:
-            break
-print(memo[0])
+@lru_cache
+def ways(i):
+    if i == len(a) - 1:
+        return 1
+    return sum([ways(j) for j in range(i + 1, len(a)) if a[j] - a[i] <= 3])
+print(ways(0))
